f7cc9ae3ead46f5e8d01023824362192a5febad6
deppdong <271772625@qq.com>
2023-03-07 00:13:50 +0800

    set

--- vue-wallet-connect/src/App.vue
+++ vue-wallet-connect/src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <status-container />
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import StatusContainer from "./components/StatusContainer.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    StatusContainer,
+  },
+};
 </script>
-
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Sora:wght@100&display=swap");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Sora", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.button {
+  background-color: #1c82ff;
+  border: none;
+  color: #ffffff;
+  font-family: "Sora";
+  border-radius: 3rem;
+  padding: 2rem 3rem;
+  font-weight: 600;
+  font-size: 2rem;
+  margin: 1rem 1rem 1rem auto;
+  width: 40%;
+}
+.disconnect__button {
+  background-color: red;
+  border: none;
+  color: #ffffff;
+  font-family: "Sora";
+  border-radius: 3rem;
+  padding: 1rem 1.3rem;
+  font-weight: 600;
+  font-size: 1rem;
+  margin: 8rem 1rem 1rem auto;
+  width: 20%;
 }
 </style>
---
+++ vue-wallet-connect/src/components/StatusContainer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>Welcome to Your Vue.js Dapp</h1>
+    <div>
+      <button class="button">Connected</button>
+      <button class="disconnect__button">Disconnect</button>
+    </div>
+
+    <button class="button">Connect Wallet</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "StatusContainer",
+};
+</script>
