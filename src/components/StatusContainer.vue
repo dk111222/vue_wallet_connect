7bfc9002f8351367a1cf0c11a7dec2bc950eb01f
dongshujian <271772625@qq.com>
2023-03-07 17:34:12 +0800

    init

---
+++ src/components/StatusContainer.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="Hi">
+      <h1>Vue.js Dapp</h1>
+      <div >
+         <button class="button">Connected</button>
+         <button class="disconnect__button">Disconnect</button>
+      </div>
+  
+      <button class="button"> Connect Wallet</button>
+    </div>
+</template>
+
+<script>
+import connect from '../composables/connect/index';
+export default {
+  name: 'StatusContainer',
+  setup: () => {
+    const { connectWalletConnect, disconnectWallet, state } = connect();
+    const connectUserWallet = async () => {
+      await connectWalletConnect();
+    };
+
+    const disconnectUser = async() => {
+      await disconnectWallet()
+    }
+    return {
+      connectUserWallet,
+      disconnectUser,
+      state
+    }
+  }
+}
+</script>
+
+
+  
